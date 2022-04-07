2b1fc175107be6181718bb444f1bad80e7a34d36
Chalo1996 <emusyoka759@gmail.com>
2022-04-07 11:29:05 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 
 /**
   * main - prints "Programming is like building a multilingual puzzle
-  * return 0
   */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
