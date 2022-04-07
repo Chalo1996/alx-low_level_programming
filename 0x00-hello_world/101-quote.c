01db4eb1817ef2dbb78e570b418743efef37207e
Chalo1996 <emusyoka759@gmail.com>
2022-04-07 15:22:56 +0000

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - print a string with special characters
+  *
+  * Return: Zero(0) for Success
+  */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
