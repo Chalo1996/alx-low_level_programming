ee0db3c6e80e557bf8fa151621b0041e35f8721a
Chalo1996 <emusyoka759@gmail.com>
2022-04-07 11:16:08 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-/**
-* puts-prints "Programming is like building a multilingual puzzle
-*/
-
 #include <stdio.h>
 
+/**
+  * main - prints "Programming is like building a multilingual puzzle
+  * return: 0 because the function is of type integer
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
