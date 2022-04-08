208ba3328e151c8143f64266be5b2d1221045396
Chalo1996 <emusyoka759@gmail.com>
2022-04-08 14:19:42 +0000

    reverse_alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - The Entry point for C code.
+  *
+  * Return: The Endpoint for C code.
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 122; c >= 97; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
