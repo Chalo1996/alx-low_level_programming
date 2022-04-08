5f1a36c4989508bb8dc1933e1c8149412988899c
Chalo1996 <emusyoka759@gmail.com>
2022-04-08 12:04:39 +0000

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - The entry point for C code
+  *
+  * Return: Zero(0) for success. The endpoint for c code
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
