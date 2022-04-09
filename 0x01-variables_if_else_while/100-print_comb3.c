585efc410354e90a3f187cf5fce8ee5c37c8d937
Chalo1996 <emusyoka759@gmail.com>
2022-04-09 21:16:19 +0000

    double_digit_comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,15 @@
   */
 int main(void)
 {
-	int i, j = 1;
+	int i = '0', j = '1';
 
-	for (i = 0; i <= 8; i++)
+	for (; i <= '8'; i++)
 	{
-		for (; j <= 9; j++)
+		for (; j <= '9'; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
+			putchar(i);
+			putchar(j);
+			if (i != '8' || j != '9')
 			{
 				putchar(',');
 				putchar(' ');
