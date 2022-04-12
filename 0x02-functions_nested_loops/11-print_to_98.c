2f514b9589646d29ade9e070f1474620f386a10b
Chalo1996 <emusyoka759@gmail.com>
2022-04-12 20:06:38 +0000

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,12 +4,11 @@
   * print_to_98 - prints all natural from n to 98.
   * @n: first parameter.
   *
-  * Return: Count.
   */
 void print_to_98(int n)
 {
 	int i;
 
 	for (i = n; i <= 98; i++)
-		return (i);
+		putchar(i);
 }
