b533fd7b17d3ee3c8fc8358c784c11ac88feb1ab
Chalo1996 <emusyoka759@gmail.com>
2022-04-12 16:54:28 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,12 @@
   */
 int print_sign(int n)
 {
-	if (n >= '0' && n <='9')
+	if (n >= 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == '0')
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
