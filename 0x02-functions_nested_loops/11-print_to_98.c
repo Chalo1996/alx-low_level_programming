51f520ddb5b8f975769a5d82ebbf11d798d57379
Chalo1996 <emusyoka759@gmail.com>
2022-04-12 20:29:26 +0000

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			if (n != 98)
 			{
 				_putchar(',');
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			if (n != 98)
 			{
 				_putchar(',');
@@ -33,7 +33,7 @@ void print_to_98(int n)
 	}
        	else
 	{
-		_putchar(n);
+		_putchar(n + '0');
 	}
 	_putchar('\n');
 }
