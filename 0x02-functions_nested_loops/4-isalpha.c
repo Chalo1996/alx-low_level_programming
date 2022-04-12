1d955155515be4e0088a27e106541d133413dc9b
Chalo1996 <emusyoka759@gmail.com>
2022-04-12 16:26:28 +0000

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
   * _isalpha - check for upper or lower case alphabet.
   * @c: First parameter.
   *
-  * Return 1 if lower or upper alphabet 0 otherwise.
+  * Return: 1 if lower or upper alphabet 0 otherwise.
   */
 int _isalpha(int c)
 {
