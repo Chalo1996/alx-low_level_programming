c32f4f2c3bcaf2ba6236ee3121596134be611a1f
Chalo1996 <emusyoka759@gmail.com>
2022-04-16 19:34:31 +0000

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,26 +14,27 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i != 0)
+		if (i == 100)
 		{
-			if ((i % 3 == 0) && (i % 5 == 0))
-			{
-				printf("FizzBuzz ");
-			}
-			else if (i % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else if (i % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("%d ", i);
-			}
+			printf("Buzz ");
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
 		}
 	}
 	printf("\n");
