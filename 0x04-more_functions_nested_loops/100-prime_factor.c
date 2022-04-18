13417e8498dee26b1554b0b0d541f93be9fe5691
Chalo1996 <emusyoka759@gmail.com>
2022-04-18 15:04:26 +0000

    100-prime_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - printf the prime factors of a number.
+  *
+  * Return: 0.
+  */
+
+int main(void)
+{
+	long int n = 612852475143;
+	int i = 2;
+	int max = 0;
+	
+	while (n != 1)
+	{
+		if (n % i == 0)
+		{
+			while (n % i == 0)
+			{
+				if (i > max)
+				{
+					max = i;
+				}
+				n = n / i;
+			}
+		}
+		i++;
+	}
+	printf("%d\n", max);
+	return (0);
+}
