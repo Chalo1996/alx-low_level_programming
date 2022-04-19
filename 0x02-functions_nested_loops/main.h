43f863f2053639431097715cbcc9e9725503b678
Chalo1996 <emusyoka759@gmail.com>
2022-04-19 14:47:33 +0000

    A header file containing the function prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
 
 int _putchar(char c);
-char _putchar(char c);
 int main(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -19,4 +16,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 int main(long int  fib);
 
-#endif
+#endif /* MAIN_H */
