91551db68294547eda9fb28ef433595976dd4cf6
Chalo1996 <emusyoka759@gmail.com>
2022-04-23 20:09:03 +0000

    copies src to dest

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _strncpy - copies scr to dest.
+  * @dest: destination.
+  * @src: source.
+  * @n: number of bytes to be copied.
+  *
+  * Return: dest.
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		if (i < n)
+		{
+			dest[i] = src[i];
+		}
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
