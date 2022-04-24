b8951637a73598566099d014025c35f1ac12ba95
Chalo1996 <emusyoka759@gmail.com>
2022-04-24 21:39:07 +0000

    prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void print_positive(int n);
+
 /**
   * print_number - prints an integer.
   * @n: number of integers to be printed.
@@ -32,24 +34,34 @@ void print_number(int n)
 	}
 	else
 	{
-		if (n / 1000000000 > 0)
-			_putchar(((n / 1000000000) % 10) + '0');
-		if (n / 100000000 > 0)
-			_putchar(((n / 100000000) % 10) + '0');
-		if (n / 10000000 > 0)
-			_putchar(((n / 10000000) % 10) + '0');
-		if (n / 1000000 > 0)
-			_putchar(((n / 1000000) % 10) + '0');
-		if (n / 100000 > 0)
-			_putchar(((n / 100000) % 10) + '0');
-		if (n / 10000 > 0)
-			_putchar(((n / 10000) % 10) + '0');
-		if (n / 1000 > 0)
-			_putchar(((n / 1000) % 10) + '0');
-		if (n / 100 > 0)
-			_putchar(((n / 100) % 10) + '0');
-		if (n / 10 > 0)
-			_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
+		print_positive(n);
 	}
+}
+
+/**
+  * print_positive - prints positive integer.
+  * @n: integer to be printed.
+  *
+  */
+void print_positive(int n)
+{
+	if (n / 1000000000 > 0)
+		_putchar(((n / 1000000000) % 10) + '0');
+	if (n / 100000000 > 0)
+		_putchar(((n / 100000000) % 10) + '0');
+	if (n / 10000000 > 0)
+		_putchar(((n / 10000000) % 10) + '0');
+	if (n / 1000000 > 0)
+		_putchar(((n / 1000000) % 10) + '0');
+	if (n / 100000 > 0)
+		_putchar(((n / 100000) % 10) + '0');
+	if (n / 10000 > 0)
+		_putchar(((n / 10000) % 10) + '0');
+	if (n / 1000 > 0)
+		_putchar(((n / 1000) % 10) + '0');
+	if (n / 100 > 0)
+		_putchar(((n / 100) % 10) + '0');
+	if (n / 10 > 0)
+		_putchar(((n / 10) % 10) + '0');
+	_putchar((n % 10) + '0');
 }
