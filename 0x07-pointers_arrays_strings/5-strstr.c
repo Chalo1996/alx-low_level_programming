ca3b9896f2777881658650a18b0f5f23ee8f238b
Chalo1996 <emusyoka759@gmail.com>
2022-04-26 18:29:22 +0000

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, k, l, len_haystack = 0, len_needle = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		len_haystack++;
+	}
+
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+		len_needle++;
+	}
+
+	for (k = 0; k < len_haystack; k++)
+	{
+		for (l = 0; l < len_needle; l++)
+		{
+			if (haystack[k + l] != needle[l])
+				break;
+		}
+		if (l == len_needle)
+			return (haystack + k);
+	}
+	return (0);
+}
