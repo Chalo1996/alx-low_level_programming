1cf067022e6d9eab20e28558b053b6b7a79c31bd
Chalo1996 <emusyoka759@gmail.com>
2022-05-12 12:30:11 +0000

    prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#ifndef LINE_H
+#define LINE_H
+
+#include <stdio.h>
+
+/**
+  * main - prints file name.
+  *
+  * Return: SUCCESS exit status.
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
+#endif /* LINE_H */
