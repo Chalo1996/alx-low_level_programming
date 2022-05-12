a0d6570ff3114482aae7d424fce3f3f1268eb292
Chalo1996 <emusyoka759@gmail.com>
2022-05-12 17:46:23 +0000

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,7 +4,5 @@
 #include "my_dog.h"
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif /* _DOG_H */
