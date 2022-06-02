d8a951640dbf4a72af6f29f657f5ad9f84814699
Chalo1996 <emusyoka759@gmail.com>
2022-06-02 20:36:36 +0000

    reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,6 +4,12 @@
 #include "main.h"
 
 /**
+  * read_textfile - reads a text file and prints it to
+  *                 the POSIX standard output.
+  * @filename: pointer to the file.
+  * @letters: size of the buffer.
+  *
+  * Return: buffer.
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
