166fbde126d6fba4909b70be1b8528731b34bca7
Chalo1996 <emusyoka759@gmail.com>
2022-06-02 21:47:08 +0000

    edited main header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#define ERROR "Error: Can't read from file %s\n"
+#define ERR "Error: Can't write to %s\n"
+
 int _putchar(char c);
-int _strlen(const char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
