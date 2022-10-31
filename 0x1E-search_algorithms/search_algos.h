4886d873f37ff65f27836885b85d8659e1706e78
Chalo1996 <emusyoka759@gmail.com>
2022-10-31 15:46:20 +0300

    linear search:

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __SEARCH_H__
+#define __SEARCH_H__
+
+/*Header files*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*prototypes*/
+int linear_search(int *array, size_t size, int value);
+
+#endif /* __SEARCH_H__ */
